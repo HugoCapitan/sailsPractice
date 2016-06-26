5cb2903b7c319e6af348786e1eaefb6cf5ef03e7
HugoCapitan <hmoravilez@gmail.com>
2016-06-25 19:59:50 -0500

    ng controller with post

--- assets/js/public/signup/SignupController.js
+++ assets/js/public/signup/SignupController.js
@@ -1,1 +1,24 @@
-angular.module('SignupModule').controller('SignupController', function(){});
+angular.module('SignupModule').controller('SignupController', ['$scope', '$http', function($scope, $http){
+
+  // Setup loading state
+  $scope.signupForm = {
+    loading: false
+  }
+
+  $scope.submitSignupForm = function() {
+    $scope.signupForm.loading = true;
+
+    $http.post('/signup', {
+      name: $scope.signupForm.name,
+      title: $scope.signupForm.title,
+      email: $scope.signupForm.email,
+      password: $scope.signupForm.password
+    })
+    .then(function onSuccess(){
+      window.locaiton = '/user'
+    })
+    .catch(function onError(sailsResponse){
+      console.log(sailsResponse);
+    });
+  }
+}]);
